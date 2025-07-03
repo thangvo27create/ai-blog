361710ae02cd75bba683152c855cb457c94bcedf
thangvo27create <landerleofric@gmail.com>
2025-07-02 18:54:23 -0700

    Update robots.txt.ts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -8,6 +8,6 @@ Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL("sitemap-index.xml", site);
+  const sitemapURL = new URL("sitemap-index.xml", "https://voidnest.vercel.app/");
   return new Response(getRobotsTxt(sitemapURL));
 };
